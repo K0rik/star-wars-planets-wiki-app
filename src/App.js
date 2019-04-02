a159d00efd1381277c31c25e787583f986dc4dc1
Jaroslav Koreiko <kor_jar@step.itstep.rv.ua>
2019-04-02 19:58:29 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
+
+class WeatherService extends Component{
+  constructor(){
+    super();
+    this.state = {
+      weatherData: null,
+    }
+  }
+
+  componentDidMount(){
+    const zip = this.props.zip;
+    const apiURL = "https://swapi.co/api/" +
+                    zip+"/";
+    fetch(apiURL)
+      .then( res => res.json() )
+      .then( json => { this.setState({weatherData: json}) })
+  }
+
+  render(){
+    const weatherData = this.state.weatherData;
+    if(!weatherData) return ( <div>Loading data...</div> );
+    return( 
+     <div>
+       <p>name:{weatherData.name}</p>
+       <p>climate: {weatherData.climate }</p>
+       <p>gravity: {weatherData.gravity}</p>
+       <p>terrain: {weatherData.terrain}</p>
+       <p>surface_water: {weatherData.surface_water}</p>
+     </div>
+    );
+  }
+}
+
+const PLACES=[
+  {name:"Planet1",zip:"1"},
+  {name:"Planet2",zip:"2"},
+  {name:"Planet3",zip:"3"},
+  {name:"Planet4",zip:"4"},
+];
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activePlace : 0,
+    }
+  }
+
   render() {
+    const activePlace = this.state.activePlace;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+         
+         {
+           PLACES.map((place,index)=>(
+             <button class='button is-primary' key={index}
+                onClick={()=>{
+                  this.setState({activePlace:index})
+                }}>
+                {place.name}             
+             </button>
+           ))}
+          <WeatherService zip={ PLACES[activePlace].zip} key={activePlace} />
       </div>
     );
   }
